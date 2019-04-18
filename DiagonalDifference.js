27a6858b8211c6fc116740410c89faa1205aa398
Swapnil_jain <Swapnil.jain@tadigital.com>
2019-04-18 11:26:47 +0530

    added diagonal difference

---
+++ DiagonalDifference.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    var l = arr.length;
+    var leftD = 0, rightD = 0;
+    for (let i = 0; i < l; i++) {
+        leftD += arr[i][i];
+    }
+    for (let i = l; i > 0; i--) {
+        rightD += arr[i - 1][l - i];
+    }
+    if (rightD > leftD) {
+        return rightD - leftD;
+    }
+    return leftD - rightD;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
